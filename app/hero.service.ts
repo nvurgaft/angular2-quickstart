0f5c22fbb4e572399cbce3245cb5329a89282743
Nick <slipperygate@gmail.com>
2016-03-10 22:44:07 +0200

    Angular 2 QuickStart
    Angular 2 QuickStart in Typescript @ https://angular.io

---
+++ app/hero.service.ts
@@ -0,0 +1,12 @@
+import {HEROES} from './mock-heroes';
+import {Injectable} from "angular2/core";
+import {Hero} from './hero';
+
+@Injectable()
+export class HeroService {
+    getHeroes() {
+        return new Promise<Hero[]>(resolve =>
+            setTimeout(()=>resolve(HEROES), 2000)
+        );
+    }
+}
